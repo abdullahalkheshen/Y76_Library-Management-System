0ed63e6e87f5f8c38fe2bcc340edaf20c3a4560a
Abdullah Al-Kheshen <abdullah.alkheshen@gmail.com>
2023-05-19 02:52:18 +0300

    Fix package.json dependency errors

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,20 +1,21 @@
-  import firebase from 'firebase/app'
-  import 'firebase/firestore'
-  import 'firebase/storage'
-  import 'firebase/auth'
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/database";
+import "firebase/compat/storage";
 
-  const config = {
-    apiKey: process.env.REACT_APP_apiKey,
-    authDomain: process.env.REACT_APP_authDomain,
-    databaseURL: process.env.REACT_APP_databaseURL,
-    projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    appId: process.env.REACT_APP_appId
-  };
-  
-  // Initialize Firebase
-  firebase.initializeApp(config);
-  
+// Your web app's Firebase configuration
+const config = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  databaseURL: process.env.REACT_APP_databaseURL,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+};
 
-  export default firebase;
+// Initialize Firebase
+firebase.initializeApp(config);
+
+export default firebase;
