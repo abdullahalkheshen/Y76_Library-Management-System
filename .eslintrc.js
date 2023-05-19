1463d3eb2f13aa80614b0ecb2554caf3f51e8bd1
abdullahalkheshen <abdullah.alkheshen@gmail.com>
2023-05-19 07:03:08 +0300

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,15 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
     },
     "extends": "eslint:recommended",
     "parserOptions": {
         "ecmaVersion": "latest",
         "sourceType": "module"
     },
-  }
+    "parser": "@eslint-parser",
+    "ecmaFeatures": {
+        "jsx": true
+    }
+}
